47e388e285f52305c49d99889ace05168ed87f47
antoniopegorelli <antoniopegorelli@gmail.com>
2022-04-18 23:10:58 -0300

    Add files via upload

---
+++ webots_simulation/controllers/epuck_go_forward/build/release/epuck_go_forward.d
@@ -0,0 +1,5 @@
+build/release/epuck_go_forward.o: epuck_go_forward.c \
+ C:\Users\anton\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\anton\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\anton\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\anton\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h
---
+++ webots_simulation/controllers/my_controller_supervisor/support_functions.py
@@ -0,0 +1,6 @@
+import random
+import pandas as pd
+
+def get_coordinates(positionX, positionY)
+    testX = positionX
+    return positionY
